8e08e6fcf379ed58d4f3efe465580e12bed34776
AlexM20 <alex.martian@yandex.ru>
2015-12-06 07:27:45 +0300

    change 1

--- app/src/main/java/com/julius/keyboard/IME.java
+++ app/src/main/java/com/julius/keyboard/IME.java
@@ -7,8 +7,13 @@ import android.media.AudioManager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
+import android.widget.Toast;
 
-public class IME extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class IME extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
 
     private KeyboardView kv;
     private Keyboard keyboard;
@@ -18,6 +23,7 @@ public class IME extends InputMethodService implements KeyboardView.OnKeyboardAc
     @Override
     public void onPress(int primaryCode) {
     }
+
     @Override
     public void onRelease(int primaryCode) {
     }
@@ -26,55 +32,74 @@ public class IME extends InputMethodService implements KeyboardView.OnKeyboardAc
     public void onKey(int primaryCode, int[] keyCodes) {
         InputConnection ic = getCurrentInputConnection();
         playClick(primaryCode);
-        switch(primaryCode){
-            case Keyboard.KEYCODE_DELETE :
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                // Toast.makeText(getBaseContext(), "НЕОПЛАЧЕНО", Toast.LENGTH_LONG).show();
                 ic.deleteSurroundingText(1, 0);
                 break;
             case Keyboard.KEYCODE_SHIFT:
-                caps = !caps;
-                keyboard.setShifted(caps);
-                kv.invalidateAllKeys();
+                // long sysTime = System.currentTimeMillis();
+               /* Date sysDate = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                Date endLicDate = new Date();
+                try {
+                    endLicDate = dateFormat.parse("15/12/2015");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (sysDate.after(endLicDate)) {
+                    // throw new IOException("License expired :-|");
+                    Toast.makeText(getBaseContext(),"License expired :-|", Toast.LENGTH_LONG).show();
+                } else {}*/
+                    //Toast.makeText(getBaseContext(), "НЕОПЛАЧЕНО", Toast.LENGTH_LONG).show();
+                    caps = !caps;
+                    keyboard.setShifted(caps);
+                    kv.invalidateAllKeys();
                 break;
             case Keyboard.KEYCODE_DONE:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
                 break;
             default:
-                char code = (char)primaryCode;
-                if(Character.isLetter(code) && caps){
+                char code = (char) primaryCode;
+                if (Character.isLetter(code) && caps) {
                     code = Character.toUpperCase(code);
                 }
-                ic.commitText(String.valueOf(code),1);
+                ic.commitText(String.valueOf(code), 1);
         }
     }
 
     @Override
     public void onText(CharSequence text) {
     }
+
     @Override
     public void swipeLeft() {
     }
+
     @Override
     public void swipeRight() {
     }
+
     @Override
     public void swipeDown() {
     }
+
     @Override
     public void swipeUp() {
     }
 
     @Override
     public View onCreateInputView() {
-        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+        kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
         keyboard = new Keyboard(this, R.layout.qwerty);
         kv.setKeyboard(keyboard);
         kv.setOnKeyboardActionListener(this);
         return kv;
     }
 
-    private void playClick(int keyCode){
-        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
-        switch(keyCode){
+    private void playClick(int keyCode) {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (keyCode) {
             case 32:
                 am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
                 break;
@@ -85,7 +110,8 @@ public class IME extends InputMethodService implements KeyboardView.OnKeyboardAc
             case Keyboard.KEYCODE_DELETE:
                 am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
                 break;
-            default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+            default:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
         }
     }
 }
